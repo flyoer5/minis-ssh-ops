a7559c143b513d7ec6c2fe296522e33512ee268d
flyoer5 <l2066284988@gmail.com>
2026-07-30 16:53:56 +0800

    test: fix China time assertions

--- app/test/time_fmt_test.dart
+++ app/test/time_fmt_test.dart
@@ -4,16 +4,18 @@ import 'package:ssh_ai_agent/util/time_fmt.dart';
 void main() {
   group('北京时间解析', () {
     test('UTC 时间转换为东八区', () {
+      final time = parseAsChina('2026-07-30T04:05:06Z');
       expect(
-        parseAsChina('2026-07-30T04:05:06Z'),
-        DateTime(2026, 7, 30, 12, 5, 6),
+        [time?.year, time?.month, time?.day, time?.hour, time?.minute, time?.second],
+        [2026, 7, 30, 12, 5, 6],
       );
     });
 
     test('带东八区偏移的时间保持北京时间', () {
+      final time = parseAsChina('2026-07-30T12:05:06+08:00');
       expect(
-        parseAsChina('2026-07-30T12:05:06+08:00'),
-        DateTime(2026, 7, 30, 12, 5, 6),
+        [time?.year, time?.month, time?.day, time?.hour, time?.minute, time?.second],
+        [2026, 7, 30, 12, 5, 6],
       );
     });
 
@@ -34,6 +36,7 @@ void main() {
         DateTime.utc(2026, 7, 30, 4, 5, 6),
       );
     });
+
     test('绝对时间使用中国常用 24 小时格式', () {
       expect(
         formatChinaAbsolute('2026-07-30T04:05:06Z'),
